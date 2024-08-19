f34d1b2302c56618ee12da0e81db0ada4ea49779
Conchy <xConchyx@gmail.com>
2024-08-19 12:36:01 +0200

    first commit

---
+++ src/main/java/org/factzoopia/sonarEvents/SonarEventsApplication.java
@@ -0,0 +1,13 @@
+package org.factzoopia.sonarEvents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarEventsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/factzoopia/sonarEvents/SonarEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.factzoopia.sonarEvents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
