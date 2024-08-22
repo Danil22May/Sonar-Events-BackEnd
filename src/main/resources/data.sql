a092efb68d110d62086ccb3ba687219b4cd6c7d0
Conchy Pereira <169025186+ConchyP@users.noreply.github.com>
2024-08-22 11:43:07 +0200

    added application properties and web config

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+/* ADMIN TABLE */
+
+INSERT INTO admins (email, password, role) 
+VALUES ('flash@gmail.com', '$2a$12$CpLpmTSK3LqKcVcD/wJaj.URGybXR0fCtLnNynPvwlNQDJT8NGH.O', 'ROLE_ADMIN');
+
+/* USERS TABLE */
+INSERT INTO users (email, password, role) 
+
+
+
+/*EVENTS TABLE */
