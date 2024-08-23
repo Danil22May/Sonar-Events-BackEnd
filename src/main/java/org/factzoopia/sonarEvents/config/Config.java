ac3f53b88ec23dbff828c7e15276c1b3a63d9b28
Conchy Pereira <169025186+ConchyP@users.noreply.github.com>
2024-08-23 11:18:00 +0200

    change confi and cors configuration

--- src/main/java/org/factzoopia/sonarEvents/config/Config.java
+++ src/main/java/org/factzoopia/sonarEvents/config/Config.java
@@ -1,24 +1,5 @@
 package org.factzoopia.sonarEvents.config;
 
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.web.servlet.config.annotation.CorsRegistry;
-// import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-// @Configuration
-// @EnableWebMvc
-// public class WebConfig implements WebMvcConfigurer {
-
-//     @Override
-//     public void addCorsMappings(CorsRegistry registry) {
-//         registry.addMapping("/api/**")
-//                 .allowedOrigins("http://localhost:5173")
-//                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-//                 .allowedHeaders("*")
-//                 .allowCredentials(true);
-//     }
-// }
-
 import java.util.Arrays;
 import org.factzoopia.sonarEvents.services.JpaUserDetailsService;
 import org.springframework.beans.factory.annotation.Value;
