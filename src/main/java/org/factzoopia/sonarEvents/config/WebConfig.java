4d4e5bd8971003ab21eb70bfafea2c50d8a3d7bb
JORGE CASTRO GUTIÉRREZ <jorgecas71@MacBook-Air-de-JORGE.local>
2024-08-27 11:15:52 +0200

    added webconfig

--- src/main/java/org/factzoopia/sonarEvents/config/WebConfig.java
+++ src/main/java/org/factzoopia/sonarEvents/config/WebConfig.java
@@ -1,27 +1,27 @@
 package org.factzoopia.sonarEvents.config;
 
-import org.hibernate.annotations.DialectOverride.OverridesAnnotation;
+import java.time.Duration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import ch.qos.logback.core.util.Duration;
-import lombok.Value;
 
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
-    
-    @Value (value = "${api.endpoint}localhost:8080/api/v1/")
+
+    @Value(value = "${api-endpoint}/**")
     private String baseUrl;
 
     @Override
-    public void addResourceHandlers(ResourceHandlersRegistry registry) {
-    
-         registry.addResourceHandlers(baseUrl)
-                .addResourceLocations("classpath:/static/","classpath:/public/")
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry.addResourceHandler(baseUrl)
+                .addResourceLocations("classpath:/static/", "classpath:/public/")
                 .setCacheControl(CacheControl.maxAge(Duration.ofDays(365)));
     }
-    
+
 }
+
