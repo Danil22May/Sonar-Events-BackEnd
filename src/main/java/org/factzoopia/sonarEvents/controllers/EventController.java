306ab3e2e3eff754d26e9c85341fcdbefba57140
Danil22May <142904608+Danil22May@users.noreply.github.com>
2024-09-02 10:01:01 +0200

    Firebase configured and dependecies added

--- src/main/java/org/factzoopia/sonarEvents/controllers/EventController.java
+++ src/main/java/org/factzoopia/sonarEvents/controllers/EventController.java
@@ -7,6 +7,7 @@ import org.factzoopia.sonarEvents.services.EventService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +16,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping(path = "${api-endpoint}/events")
@@ -27,38 +30,42 @@ public class EventController {
     @Autowired
     public EventController(EventService eventService) {
         this.eventService = eventService;
-}
+    }
 
-@GetMapping(path = "/all")
-  public ResponseEntity<List<Event>> getAllEvents() {
+    @GetMapping(path = "/all")
+    public ResponseEntity<List<Event>> getAllEvents() {
         List<Event> events = eventService.getAllEvents();
         return ResponseEntity.ok(events);
-}
+    }
 
-   @GetMapping(path = "/{id}")
+    @GetMapping(path = "/{id}")
     public ResponseEntity<Event> getEventById(@PathVariable Long id) {
         Event event = eventService.getEventById(id);
         if (event != null) {
-          return ResponseEntity.ok(event);
+            return ResponseEntity.ok(event);
         } else {
             return ResponseEntity.notFound().build();
         }
     }
 
-  @PostMapping("/create")
-    public ResponseEntity<Event> createEvent(@RequestBody Event event) {
+    @PostMapping("/create")
+    public ResponseEntity<Event> createEvent(@RequestBody Event event, @RequestParam("image") MultipartFile file) {
+        if (file.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+
         Event createdEvent = eventService.createEvent(event);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdEvent);
     }
 
-      @PutMapping(path = "/{id}")
+    @PutMapping(path = "/{id}")
     public ResponseEntity<Event> uptadeEvent(@RequestBody Event event, @PathVariable Long id) {
         event.setId(id);
         Event updateEvent = eventService.updateEvent(event);
         return ResponseEntity.ok(updateEvent);
     }
 
-      @DeleteMapping(path = "/{id}")
+    @DeleteMapping(path = "/{id}")
     public ResponseEntity<Void> deleteEvent(@PathVariable Long id) {
         eventService.deleteEventById(id);
 
@@ -84,7 +91,7 @@ public class EventController {
     }
 
     // Endpoint para tener todos los eventos pasados y disponibles
-    
+
     @GetMapping("/past")
     public ResponseEntity<List<Event>> getPastEvents() {
         List<Event> pastEvents = eventService.getPastEvents();
